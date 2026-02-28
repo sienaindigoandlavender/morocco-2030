290eb70b0d770255fd67a1b2dac9ae0de4bd72ae
sienaindigoandlavender <siena@indigoandlavender.love>
2026-02-28 22:17:39 +0000

    Add files via upload

---
+++ lib/images.ts
@@ -0,0 +1,50 @@
+// All Midjourney-generated images hosted on Cloudinary
+// Style: documentary editorial, --style raw --s 70, muted luminous, grain 8%
+
+export const images = {
+  hero: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772311698/Aerial_view_of_Moroccan_landscape_fqmk5w.png',
+
+  marrakech: {
+    hero: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772311844/Marrakech_medina_rooftops_at_dusk_qdoyej.png',
+    zellige: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772312026/hand-cut_Moroccan_zellige_tilework_nw0u5i.png',
+    benYoussef: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772312218/Interior_courtyard_of_Ben_Youssef_ol0ps0.png',
+  },
+  casablanca: {
+    hero: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772312493/Hassan_II_Mosque_Casablanca_f2wasz.png',
+    artDeco: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772313973/Art_deco_building_facade_Boulevard_Mohammed_V__casablanca_mrc4bi.png',
+    corniche: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772314110/Casablanca_corniche_two_fishermen_on_Atlanticw_fqqqpz.png',
+  },
+  rabat: {
+    hero: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772314226/Kasbah_of_the_Udayas_Rabat_y8rd2i.png',
+    chellah: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772314355/Chellah_necropolis_Rabat_Roman_columns_kzqm6d.png',
+    hassanTower: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772314412/Hassan_Tower_Rabat_ehlg9k.png',
+  },
+  tangier: {
+    hero: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772314478/View_from_Caf_Hafa_Tangier_huuuyh.png',
+    medinaAlley: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772314562/Narrow_alley_in_Tangier_medina_fyhk0w.png',
+    capeSpartel: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772314640/Cape_Spartel_lighthouse_Tangier_ivrlhv.png',
+  },
+  fes: {
+    hero: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772314702/Fes_el-Bali_medina_lx6wib.png',
+    tanneries: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772314770/Chouara_tanneries_Fes_hrg3lz.png',
+    madrasa: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772314846/Bou_Inania_Madrasa_Fes_jwlnss.png',
+    donkey: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772314922/Donkey_carrying_leather_hides_prnjui.png',
+  },
+  agadir: {
+    hero: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772315001/Agadir_beach_at_dusk_zn4kj4.png',
+    taghazout: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772315067/Single_surfer_riding_a_right-hand_point_break_b4ycz1.png',
+    paradiseValley: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772315131/Paradise_Valley_Morocco_i225hf.png',
+  },
+
+  food: {
+    tagine: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772315252/Traditional_Moroccan_tagine_lamb_with_prunes_hp5dmt.png',
+    mintTea: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772315324/Moroccan_mint_tea_being_poured_from_height_sw0v88.png',
+    jemaaStalls: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772315401/Night_food_stalls_Jemaa_el-Fna_Marrakech_smoke_hfxjwq.png',
+  },
+
+  atmosphere: {
+    medinaDooor: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772315539/Unmarked_wooden_door_in_Marrakech_medina_alley_nd3tzr.png',
+    trainWindow: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772315645/View_from_inside_Al_Boraq_high-speed_train_qvbyyv.png',
+    hammam: 'https://res.cloudinary.com/drstfu5yr/image/upload/v1772315888/Interior_of_traditional_Moroccan_hammam_te9qxa.png',
+  },
+} as const;
